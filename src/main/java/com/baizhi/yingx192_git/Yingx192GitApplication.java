7218f216efb1cf9abbb9e8c4ef19c1b78793e018
zhangcn <15236674712@163.com>
2020-07-16 09:59:36 +0800

    应学项目第一个版本提交  v1.0

---
+++ src/main/java/com/baizhi/yingx192_git/Yingx192GitApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.yingx192_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yingx192GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yingx192GitApplication.class, args);
+    }
+
+}
